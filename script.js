7f0cbd5f40acc3d881486eb690057945b56dc103
Tristan Bond <tristanebond@gmail.com>
2020-10-18 15:17:27 -0700

    bug fixed, now successfully generates passwords but not meeting all requirments

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
   // get password length
   var charLength=prompt("How many characters would you like in your password?");
-  var lowerCaseArr=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-  var upperCaseArr=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+  var lowerCaseArr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCaseArr=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var numArr=["0","1","2","3","4","5","6","7","8","9"];
   var specialCharArr=["!","$","%","#","*"];
   var passwordArr=[];
@@ -40,29 +40,29 @@ function generatePassword(){
     while(counter<passwordArr.length){
       // create random lowercase letter in random position
       if(lowerCase){
-       password.Arr[Math.floor(Math.random()*passwordArr.length)]=lowerCaseArr[Math.floor(Math.random()*lowerCaseArr.length)];
+      passwordArr[Math.floor(Math.random()*passwordArr.length)]=lowerCaseArr[Math.floor(Math.random()*lowerCaseArr.length)];
       counter++;
       }
       //create random uppercase letter in random position
       if(upperCase){
-        password.Arr[Math.floor(Math.random()*passwordArr.length)]=upperCaseArr[Math.floor(Math.random()*upperCaseArr.length)];
+        passwordArr[Math.floor(Math.random()*passwordArr.length)]=upperCaseArr[Math.floor(Math.random()*upperCaseArr.length)];
       counter++;
       }
       //create random number in random position
       if(numeric){
-        password.Arr[Math.floor(Math.random()*passwordArr.length)]=numArr[Math.floor(Math.random()*numArr.length)];
+        passwordArr[Math.floor(Math.random()*passwordArr.length)]=numArr[Math.floor(Math.random()*numArr.length)];
       counter++;
       }
       //create random special character in random position
       if(specialChar){
-        password.Arr[Math.floor(Math.random()*passwordArr.length)]=specialCharArr[Math.floor(Math.random()*specialCharArr.length)];
+        passwordArr[Math.floor(Math.random()*passwordArr.length)]=specialCharArr[Math.floor(Math.random()*specialCharArr.length)];
       counter++;
       }
     }
     for(var i=0;i<passwordArr.length-1;i++){
-      password+=password.Arr[i];
+      password+=passwordArr[i];
     }
-    alert(password);
+    return password;
 
 
 }
